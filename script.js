0d1af88f4a709fe73e0478f5f20d8b958d8e5584
giovmahe <giovannimaendra@gmail.com>
2021-01-22 13:54:30 +0700

    Add top button, header button, mouseover(out)

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
+//Declare variable
 const bioName = document.getElementsByClassName('bioName')
 const ayoBerteman = document.getElementsByClassName('ayoBerteman')[0]
-const button = document.getElementsByClassName('button')[0]
+const buttonKlik = document.getElementsByClassName('buttonKlik')[0]
+const buttonTop = document.getElementById('buttonTop')
+const listGroup = document.getElementById('listGroup')
 
-console.log(button);
+// console.log(listGroup);
 
-button.onclick = () => {
+//make button that response onclik at the header
+buttonKlik.onclick = () => {
     ayoBerteman.innerHTML = 'Ayo Berteman!';
+    buttonKlik.disabled = true;
+}
+
+//make top button appear, when user scrool the page 40px
+window.onscroll = () => {
+    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+        buttonTop.style.display = "block";
+        // ayoBerteman.innerHTML = "<br>";
+    } else {
+        buttonTop.style.display = "none";
+    }
+
+}
+
+//make button Top response, and user will go to the top of the page
+buttonTop.onclick = () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+//the list will change color, when mouseover and mouseout it
+listGroup.onmouseover = () => {
+    listGroup.style.color = "#8e8d11";
+}
+
+listGroup.onmouseout = () => {
+    listGroup.style.color = "#E4E3B6";
 }
